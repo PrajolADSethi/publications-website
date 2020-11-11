8cd529b67560f06c1f4e937d0a92c408abfe56ec
Naitik0909 <parmarnaitik0909@gmail.com>
2020-11-11 17:53:58 +0530

    Modified models

--- api_app/models.py
+++ api_app/models.py
@@ -1,51 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class College(models.Model):
+
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     clgID = models.BigIntegerField(blank=False,null=False) #doubt
     website = models.URLField(max_length=200,**options)
 
-class Departments(models.Model):
+class Department(models.Model):
+
+    collegeID = models.ForeignKey(College, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     deptID = models.BigIntegerField(blank=False,null=False)
 
-class Author(models,model):
+class Author(models.Model):
+
+    deptartment = models.ForeignKey(Department, on_delete=models.SET_NULL)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # college = models.OneToOneField(College, on_delete=models.SET_NULL)
     name = model.CharField(max_length=100)
     userID = models.BigIntegerField(blank=False,null=False)
-    ORCid = models.UUIDField(
-          primary_key = True,
-          default = uuid.uuid4,
-          editable = False)
-    scopusID= models.UUIDField(
-          primary_key = True,
-          default = uuid.uuid4,
-          editable = False)
+    ORCid = models.BigIntegerField()
+    scopusID= models.BigIntegerField()
     bio = models.CharField(max_length=1000)
     affiliation = models.CharField(max_length=100)
     email = models.EmailField(blank=True,null=True)
     phoneno = models.BigIntegerField(blank=True,null=True)
     weblinks = models.URLField(max_length=200,**options)
     designation = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = 
+    # username = models.CharField(max_length=100)
+    # password =
 
 
 
 class Paper(models.Model):
+    authorID = models.ForeignKey(Author, on_delete=models.SET_NULL)
     title = models.CharField(max_length=100)
-    DOI = models.UUIDField(
-          primary_key = True,
-          default = uuid.uuid4,
-          editable = False)
+    DOI = models.CharField(max_length=50, primary_key=True, unique=True)
     pri_author = models.CharField(max_length=100)
-    co_authors
-    volume
-    issue
-    citation = imodels.AutoField(primary_key=True, **options)
-    field = models.CharField(max_length=100)
+    citation = models.IntegerField()
+    fields = models.CharField(max_length=100)
     publication_date = models.TimeField(auto_now=False, auto_now_add=False, **options)
     publish_name = models.CharField(max_length=100)
     abstract = models.CharField(max_length=1000)
-    tags =
+    # tags =
