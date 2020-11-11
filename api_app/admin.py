84f10e8650e4fc034e6080a81cba0bbf3cdc7123
PrajolADSethi <51747460+PrajolADSethi@users.noreply.github.com>
2020-11-11 18:55:36 +0530

    model testing

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import College , Author , Department , Paper
+admin.site.register(College)
+admin.site.register(Author)
+admin.site.register(Department)
+admin.site.register(Paper)
 
 # Register your models here.
--- api_app/models.py
+++ api_app/models.py
@@ -7,43 +7,50 @@ class College(models.Model):
 
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
-    clgID = models.BigIntegerField(blank=False,null=False) #doubt
-    website = models.URLField(max_length=200,**options)
+    website = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Department(models.Model):
 
     collegeID = models.ForeignKey(College, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    deptID = models.BigIntegerField(blank=False,null=False)
+    # deptID = models.BigIntegerField(blank=False,null=False)
+
+    def __str__(self):
+        return f'{self.collegeID.name} => {self.name}'
 
 class Author(models.Model):
 
-    deptartment = models.ForeignKey(Department, on_delete=models.SET_NULL)
+    deptartment = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # college = models.OneToOneField(College, on_delete=models.SET_NULL)
-    name = model.CharField(max_length=100)
-    userID = models.BigIntegerField(blank=False,null=False)
-    ORCid = models.BigIntegerField()
-    scopusID= models.BigIntegerField()
+    name = models.CharField(max_length=100)
+    ORCid = models.CharField(max_length=19,null=True,blank=True)
+    scopusID= models.BigIntegerField(null=True, blank=True)
     bio = models.CharField(max_length=1000)
     affiliation = models.CharField(max_length=100)
     email = models.EmailField(blank=True,null=True)
     phoneno = models.BigIntegerField(blank=True,null=True)
-    weblinks = models.URLField(max_length=200,**options)
+    weblinks = models.CharField(max_length=200 ,null=True,blank=True)
     designation = models.CharField(max_length=100)
     # username = models.CharField(max_length=100)
     # password =
-
+    def __str__(self):
+        return self.name
 
 
 class Paper(models.Model):
-    authorID = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    authorID = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=100)
     DOI = models.CharField(max_length=50, primary_key=True, unique=True)
     pri_author = models.CharField(max_length=100)
     citation = models.IntegerField()
     fields = models.CharField(max_length=100)
-    publication_date = models.TimeField(auto_now=False, auto_now_add=False, **options)
+    publication_date = models.TimeField(auto_now=False, auto_now_add=False)
     publish_name = models.CharField(max_length=100)
     abstract = models.CharField(max_length=1000)
     # tags =
+    def __str__(self):
+        return self.title
